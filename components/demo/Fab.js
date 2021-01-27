3ed4983a21470e03b24bce0aa26e51cb338f9297
Danilo Leal <daniloleal09@gmail.com>
2021-01-27 19:11:34 -0300

    fix fab position mobile

--- components/demo/Fab.js
+++ components/demo/Fab.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Fab() {
   return (
     <Link href="">
-      <div className="w-16 h-16 bg-blue-500 rounded-full p-3 flex items-center justify-center shadow-lg hover:bg-blue-700 hover:shadow-xl cursor-pointer transition-all fixed bottom-8 right-8">
+      <div className="w-16 h-16 bg-blue-500 rounded-full p-3 flex items-center justify-center shadow-lg hover:bg-blue-700 hover:shadow-xl cursor-pointer transition-all fixed sm:bottom-8 sm:right-8 bottom-4 right-4">
         <svg
           className="w-6 h-6 stroke-current text-white"
           fill="none"
