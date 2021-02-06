9a9e05021dd51dd9461773a700b1ba4d8efb6c74
Danilo Leal <daniloleal09@gmail.com>
2021-02-05 21:57:11 -0300

    why isnt sticky not working

--- components/ChangeLogFilter.js
+++ components/ChangeLogFilter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ChangeLogFilter() {
   return (
-    <div className="w-full inline-flex overflow-x-auto space-x-2 sm:overflow-visible sm:flex sm:flex-col sm:space-x-0 bg-white dark:bg-blackish py-4 sm:py-0">
+    <div className="w-full inline-flex overflow-x-auto space-x-2 py-4 bg-white dark:bg-blackish sm:overflow-visible sm:flex sm:flex-col sm:space-x-0 sm:py-0">
       <div className="p-3 w-full hover:bg-gray-100 dark:hover:bg-blackish2 transition-all rounded-full min-w-max">
         <div className="flex space-x-4 items-center">
           <div className="w-2 h-2 rounded-full bg-gray-700 dark:bg-gray-500"></div>
--- components/ChangeLogWrapper.js
+++ components/ChangeLogWrapper.js
@@ -5,7 +5,7 @@ import ChangeLogFilter from './ChangeLogFilter';
 export default function ChangeLogWrapper() {
   return (
     <div className="relative py-8 sm:py-16 sm:grid sm:grid-cols-3 sm:gap-8">
-      <div className="sticky top-0 pb-8 sm:pb-0">
+      <div className="sticky sm:top-4 top-0 pb-8 sm:pb-0">
         <ChangeLogFilter />
       </div>
       <div className="col-span-2 flex flex-col space-y-8">
