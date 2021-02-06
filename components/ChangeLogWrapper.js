59caf4253c076a28f262e3280df044a11c3b8cac
Danilo Leal <daniloleal09@gmail.com>
2021-02-05 21:51:23 -0300

    more work on changelog

--- components/ChangeLogWrapper.js
+++ components/ChangeLogWrapper.js
@@ -4,8 +4,8 @@ import ChangeLogFilter from './ChangeLogFilter';
 
 export default function ChangeLogWrapper() {
   return (
-    <div className="relative sm:grid sm:grid-cols-3 sm:gap-8">
-      <div className="sticky top-0">
+    <div className="relative py-8 sm:py-16 sm:grid sm:grid-cols-3 sm:gap-8">
+      <div className="sticky top-0 pb-8 sm:pb-0">
         <ChangeLogFilter />
       </div>
       <div className="col-span-2 flex flex-col space-y-8">
--- pages/change-log.js
+++ pages/change-log.js
@@ -6,16 +6,14 @@ export default function ChangeLog() {
   return (
     <div className="large-container sm:px-6 sm:pt-6 sm:pb-24 px-4 pt-4">
       <Navbar />
-      <div className="h-80 flex flex-col items-center pt-24">
+      <div className="h-64 sm:h-80 flex flex-col items-center pt-24">
         <div className="flex flex-col space-y-2 items-center">
           <h1 className="h1">Changelog</h1>
           <p className="p">Histórico de atualizações do Coop :)</p>
         </div>
       </div>
       <div className="w-full h-0.5 bg-gray-100 dark:bg-blackish2"></div>
-      <div className="py-16">
-        <ChangeLogWrapper />
-      </div>
+      <ChangeLogWrapper />
       <Footer />
     </div>
   );
